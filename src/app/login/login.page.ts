a1808daa8b514ee36ee9c968c07f9378ee655552
Carlos <charlitosf@gmail.com>
2020-04-28 17:36:14 +0200

    Tercer Taller completado (pero no bonito)

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  miForm: any;
+  login: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.miForm = this.fb.group({
+      emailControl: ['', [Validators.required, Validators.email]],
+      passwordControl: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  doLogin() {
+    if (this.miForm.dirty && this.miForm.valid) {
+      console.log("Email: " + this.miForm.value.emailControl);
+      console.log("Password: " + this.miForm.value.passwordControl);
+      this.login = true;
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
